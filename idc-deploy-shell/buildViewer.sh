094de3d7e59d16a0a64aa19b14ea79f0a82cb7b2
wlongabaugh <wlongabaugh@systemsbiology.org>
2021-07-20 18:35:24 -0700

    Real deploy

---
+++ idc-deploy-shell/buildViewer.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cd slim
+
+if [ "${CONFIG_ONLY}" != "True" ]; then
+  yarn install --frozen-lockfile --network-timeout 100000
+  # Was 8192, but we want to run this on an 8 GB machine:
+  export NODE_OPTIONS=--max_old_space_size=6144
+  yarn run build
+else
+  mkdir -p build/config/
+fi
