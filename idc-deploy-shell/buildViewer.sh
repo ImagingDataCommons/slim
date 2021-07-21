7f46a47e103192f81bd5d623cf49e41f01cf679e
wlongabaugh <wlongabaugh@systemsbiology.org>
2021-07-20 21:08:44 -0700

    Trying to get build working III

--- idc-deploy-shell/buildViewer.sh
+++ idc-deploy-shell/buildViewer.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "${CONFIG_ONLY}" != "True" ]; then
-  yarn install --immutable --network-timeout 100000
+  yarn install --frozen-lockfile --network-timeout 100000
   # Was 8192, but we want to run this on an 8 GB machine:
   export NODE_OPTIONS=--max_old_space_size=6144
   yarn run build
