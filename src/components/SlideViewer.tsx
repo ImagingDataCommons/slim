d749e22fec624521a5c055fae0d0f00ab3017622
hackermd <hackermd@protonmail.com>
2021-08-13 17:43:57 -0400

    Fix typo in error message

--- src/components/SlideViewer.tsx
+++ src/components/SlideViewer.tsx
@@ -451,7 +451,7 @@ class SlideViewer extends React.Component<SlideViewerProps, SlideViewerState> {
             this.setState({ isLoading: false })
           }).catch((error) => {
             // eslint-disable-next-line @typescript-eslint/no-floating-promises
-            message.error('An error occured. Annotation could not be loaded')
+            message.error('An error occured. Annotations could not be loaded')
             console.error(error)
           })
         }
