4ab9803f751c46eec459e234489fe2b60afb6afc
hackermd <hackermd@protonmail.com>
2021-10-15 09:41:06 -0400

    Hardcode URL of homepage
    Create React App mangles the `homepage` variable defined in
    `package.json`.

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import './index.css'
 import AppConfig from './AppConfig'
 import App from './App'
 
-import { name, homepage, version } from '../package.json'
+import { name, version } from '../package.json'
 
 declare global {
   interface Window {
@@ -25,7 +25,7 @@ ReactDOM.render(
       config={config}
       version={version}
       name={name}
-      homepage={homepage}
+      homepage='https://github.com/MGHComputationalPathology/slim'
     />
   </React.StrictMode>,
   document.getElementById('root')
