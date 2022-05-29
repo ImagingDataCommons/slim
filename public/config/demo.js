ca5b5a8c838b89878d332a0f7bc5a4726ba51680
hackermd <hackermd@protonmail.com>
2022-05-28 21:12:11 -0400

    Update dicomweb url for demo configuration

--- public/config/demo.js
+++ public/config/demo.js
@@ -3,7 +3,7 @@ window.config = {
   servers: [
     {
       id: 'demo',
-      url: 'https://idc-external-006.uc.r.appspot.com',
+      url: 'https://idc-external-006.uc.r.appspot.com/dcm4chee-arc/aets/DCM4CHEE/rs',
       write: false
     }
   ],
