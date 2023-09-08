9042b987363ea7ef4e3bc14b60b7ff92d816f0da
Igor Octaviano <igoroctaviano@gmail.com>
2023-09-08 07:22:43 -0300

    Add new annotation property

--- src/components/SlideViewer.tsx
+++ src/components/SlideViewer.tsx
@@ -180,6 +180,9 @@ const _constructViewers = ({ clients, slide, preload }: {
     const volumeViewer = new dmv.viewer.VolumeImageViewer({
       clientMapping: clients,
       metadata: slide.volumeImages,
+      annotationOptions: {
+        maxZoom: 2.8,
+      },
       controls: ['overview', 'position'],
       preload: preload,
       errorInterceptor: (error: CustomError) =>
