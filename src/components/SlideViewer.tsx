2269d5084e3c91c5bc119ab4dc6d043b4c78b33c
Pedro Kohler <pedrokohlerbh@gmail.com>
2025-03-21 13:07:13 -0300

    fix: formatAnnotation type

--- src/components/SlideViewer.tsx
+++ src/components/SlideViewer.tsx
@@ -3177,7 +3177,7 @@ class SlideViewer extends React.Component<SlideViewerProps, SlideViewerState> {
     this.volumeViewer.toggleICCProfiles()
   }
 
-  formatAnnotation = (annotation: any) => {
+  formatAnnotation = (annotation: AnnotationCategoryAndType | dmv.roi.ROI) => {
     const roi = this.volumeViewer.getROI(annotation.uid)
     const key = _getRoiKey(roi) as string
     const color = this.roiStyles[key] !== undefined
