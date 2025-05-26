f7a053c497d337e8bb297cd1de6b5f02f0f5eba7
Igor Octaviano <igoroctaviano@gmail.com>
2025-05-26 18:57:00 -0300

    Use rewrite

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,6 @@
 const CracoLessPlugin = require('craco-less')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
   plugins: [
@@ -43,7 +44,6 @@ module.exports = {
         }
       }
       config.plugins.push(
-        // TO DO: remove hard coded path
         new CopyWebpackPlugin({
           patterns: [
             {
@@ -53,6 +53,28 @@ module.exports = {
           ]
         })
       )
+
+      // Rewrite dicom-microscopy-viewer URLs to correct path
+      config.plugins.push({
+        apply: (compiler) => {
+          compiler.hooks.compilation.tap('RewriteUrlsPlugin', (compilation) => {
+            compilation.hooks.processAssets.tap(
+              { name: 'RewriteUrlsPlugin', stage: webpack.Compilation.PROCESS_ASSETS_STAGE_OPTIMIZE },
+              (assets) => {
+                for (const [filename, asset] of Object.entries(assets)) {
+                  if (filename.endsWith('.js') && typeof asset.source() === 'string') {
+                    compilation.updateAsset(
+                      filename,
+                      new webpack.sources.RawSource(asset.source().replace(/\/dicom-microscopy-viewer\//g, '/static/js/'))
+                    );
+                  }
+                }
+              }
+            );
+          });
+        }
+      });
+
       config.target = 'web'
       config.experiments = {
         asyncWebAssembly: true
