201606cea06fc7f1384a90adc5414b643922c52b
Igor Octaviano <igoroctaviano@gmail.com>
2025-05-26 20:09:34 -0300

    fix: dmv imports

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,6 @@
 const CracoLessPlugin = require('craco-less')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
   plugins: [
@@ -53,6 +54,25 @@ module.exports = {
           ]
         })
       )
+      config.plugins.push({
+        apply: (compiler) => {
+          compiler.hooks.compilation.tap('RewriteUrlsPlugin', (compilation) => {
+            compilation.hooks.processAssets.tap(
+              { name: 'RewriteUrlsPlugin', stage: webpack.Compilation.PROCESS_ASSETS_STAGE_OPTIMIZE },
+              (assets) => {
+                for (const [filename, asset] of Object.entries(assets)) {
+                  if (typeof asset.source() === 'string') {
+                    compilation.updateAsset(
+                      filename,
+                      new webpack.sources.RawSource(asset.source().replace(/\/dicom-microscopy-viewer\//g, '/static/js/'))
+                    );
+                  }
+                }
+              }
+            )
+          })
+        }
+      })
       config.target = 'web'
       config.experiments = {
         asyncWebAssembly: true
