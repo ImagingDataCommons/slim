a797cda07eb07d73e3e5f8457f5ef417cad5baab
Igor Octaviano <igoroctaviano@gmail.com>
2025-08-07 12:33:57 -0300

    Reset visibility on series selection

--- src/components/SlideViewer.tsx
+++ src/components/SlideViewer.tsx
@@ -636,7 +636,7 @@ class SlideViewer extends React.Component<SlideViewerProps, SlideViewerState> {
       console.debug('TODO: Loading Advanced Blending Presentation State')
     } else if ((derivedDataset as { SOPClassUID: string }).SOPClassUID === ColorSoftcopyPresentationState) {
       console.debug('TODO: Loading Color Softcopy Presentation State')
-    } else if ((derivedDataset as { SOPClassUID: string }).SOPClassUID === GrayscaleSoftcopyPresentationState) {  
+    } else if ((derivedDataset as { SOPClassUID: string }).SOPClassUID === GrayscaleSoftcopyPresentationState) {
       console.debug('TODO: Loading Grayscale Softcopy Presentation State')
     } else if ((derivedDataset as { SOPClassUID: string }).SOPClassUID === PseudocolorSoftcopyPresentationState) {
       console.debug('TODO: Loading Pseudocolor Softcopy Presentation State')
@@ -2698,7 +2698,16 @@ class SlideViewer extends React.Component<SlideViewerProps, SlideViewerState> {
   }
 
   handleAnnotationGroupSelection = (value: string): void => {
-    this.setState({ selectedSeriesInstanceUID: value })
+    // Hide all currently visible annotation groups when selection changes
+    this.state.visibleAnnotationGroupUIDs.forEach(annotationGroupUID => {
+      this.volumeViewer.hideAnnotationGroup(annotationGroupUID)
+    })
+
+    // Reset the visible annotation groups state
+    this.setState({
+      selectedSeriesInstanceUID: value,
+      visibleAnnotationGroupUIDs: new Set()
+    })
   }
 
   getSeriesDescription = (seriesInstanceUID: string): string => {
